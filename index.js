a94ff11f7b8c68e36ccd2cbe5913194a30cb1e7a
Macbook <macbook@192.168.2.5>
2026-05-19 07:50:10 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const resultBox = document.getElementById("resultBox");
+
+function showMessage(){
+    resultBox.innerHTML = "Delhii deerh hamgiin onodr uul bol Everest yum";
+    }
---
+++ index02.js
@@ -0,0 +1,16 @@
+const resultBox = document.getElementById("result-box");
+
+function showCake() {
+  resultBox.innerHTML =
+    "🍬 Таны чихрийн захиалга\nхүлээн авлаа!";
+}
+
+function showCandy() {
+  resultBox.innerHTML =
+    "🎂 Таны бялууны захиалга\nхүлээн авлаа!";
+}
+
+function showIceCream() {
+  resultBox.innerHTML =
+    "🍦 Таны зайрмагийн захиалга\nхүлээн авлаа!";
+}
